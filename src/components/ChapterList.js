1d2539a0f41eee5cba9fbe8cf595d9dcf4b54bbb
pri1311 <nagdapriya13@gmail.com>
2021-12-31 00:02:11 +0530

    Parse description

--- src/components/ChapterList.js
+++ src/components/ChapterList.js
@@ -1,14 +1,24 @@
 import React, { useEffect } from 'react';
 import axios from "axios";
+import { useSelector } from 'react-redux';
+
 
 
 function ChapterList() {
-    var key = process.env.REACT_YOUTUBE_API_KEY;
-    var videoID = 'k68j9xlbHHk'
-    var url = 'https://www.googleapis.com/youtube/v3/videos'
+    var key = process.env.REACT_APP_YOUTUBE_API_KEY;
+    var baseURL = 'https://www.googleapis.com/youtube/v3/videos';
+    var { url } = useSelector((state) => state.player.value);
+
+    function getVideoId(url) {
+        let regex = /https\:\/\/www\.youtube\.com\/watch\?v=([\w-]{11})/;
+
+        const id = url.match(regex)[1]; 
+        return id;
+    }
 
     function loadDescription() {
-        axios.get(url, {
+        let videoID = getVideoId(url);
+        axios.get(baseURL, {
         params: {
             part: "snippet",
             key: key,
@@ -16,8 +26,32 @@ function ChapterList() {
         },
         })
         .then((result) => {
-            console.log(result.data.items[0].snippet.description);
+            console.log(parseDescription(result.data.items[0].snippet.description));
         })
+        .catch((err)=> {console.log(err);})
+    }
+
+    function parseDescription(desc) {
+        var list_of_chapters = []
+        var file = []
+
+        file = desc.split('\n');
+        for (var l in file) {
+            var line = (file[l]).trim()
+            var result = "";
+            var chapter = "";
+            console.log(line);
+            result = line.match(/\(?(\d+[:]\d+[:]\d+)\)?/)
+
+            if (result === null) {
+                result = line.match(/\(?(\d+[:]\d+)\)?/);
+            }   
+
+            if (result === null) continue;
+            chapter = line.split(result[1])
+            list_of_chapters.push([result[1], chapter[1]])
+        }
+        return list_of_chapters;
     }
 
     useEffect(() => {
